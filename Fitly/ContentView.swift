ae90a266339e3d0a0dbfbccc7883de285a57edc7
visortix <visortix@gmail.com>
2026-02-08 19:07:10 +0200

    Initial Commit

---
+++ Fitly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fitly
+//
+//  Created by visortix on 08.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fitly/FitlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitlyApp.swift
+//  Fitly
+//
+//  Created by visortix on 08.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct FitlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
